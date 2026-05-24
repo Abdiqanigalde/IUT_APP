54db3e81c9e961a7a0b2168b0a04a216e5d6822a
Abdiqanigalde <154256631+Abdiqanigalde@users.noreply.github.com>
2026-05-25 01:37:50 +0600

    Update app.py

--- app.py
+++ app.py
@@ -36,10 +36,10 @@ app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 
 # ── Flask-Mail ────────────────────────────────────────────────────────────────
-app.config['MAIL_SERVER']         = 'smtp.gmail.com'
-app.config['MAIL_PORT']    = 465
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_SERVER']  = 'smtp-relay.brevo.com'
+app.config['MAIL_PORT']    = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME']       = os.environ.get('MAIL_USERNAME', '')
 app.config['MAIL_PASSWORD']       = os.environ.get('MAIL_PASSWORD', '')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME', 'noreply@iut-dhaka.edu')
